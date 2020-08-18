6adc804218fa8b2ff7fc25032067bfc231777721
UrucXay <urucxay@gmail.com>
2020-08-18 16:12:31 +0300

    Add new extensions for mapping.

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/data/ArticleState.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/data/ArticleState.kt
@@ -1,13 +1,17 @@
 package ru.skillbranch.skillarticles.extensions.data
 
 import ru.skillbranch.skillarticles.data.models.AppSettings
-import ru.skillbranch.skillarticles.data.models.ArticlePersonalInfo
+import ru.skillbranch.skillarticles.data.repositories.ArticleFilter
 import ru.skillbranch.skillarticles.viewmodels.article.ArticleState
+import ru.skillbranch.skillarticles.viewmodels.articles.ArticlesState
 
-fun ArticleState.toAppSettings(): AppSettings {
-    return AppSettings(isDarkMode, isBigText)
+fun ArticleState.toAppSettings() : AppSettings {
+    return AppSettings(isDarkMode,isBigText)
 }
 
-fun ArticleState.toArticlePersonalInfo(): ArticlePersonalInfo {
-    return ArticlePersonalInfo(isLike, isBookmark)
-}
+fun ArticlesState.toArticleFilter(): ArticleFilter = ArticleFilter(
+    search = searchQuery,
+    isBookmark = isBookmark,
+    categories = selectedCategories,
+    isHashtag = isHashtagSearch
+)
