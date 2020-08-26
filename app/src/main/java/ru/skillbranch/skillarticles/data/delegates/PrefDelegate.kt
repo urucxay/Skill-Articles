e3b2a0e232a70cba4b33d33c289be3b2f5631bac
UrucXay <urucxay@gmail.com>
2020-08-26 10:44:43 +0300

    Fix for tests

--- app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
@@ -1,6 +1,9 @@
 package ru.skillbranch.skillarticles.data.delegates
 
+import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
 import ru.skillbranch.skillarticles.data.local.PrefManager
+import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -33,4 +36,59 @@ class PrefDelegate<T>(private val defaultValue: T) : ReadWriteProperty<PrefManag
             apply()
         }
     }
+}
+
+class PrefLiveDelegate<T>(
+    private val fieldKey: String,
+    private val defaultValue: T,
+    private val preferences: SharedPreferences
+) : ReadOnlyProperty<Any?, LiveData<T>> {
+
+    private var storedValue: LiveData<T>? = null
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): LiveData<T> {
+        if (storedValue == null) {
+            storedValue = SharedPreferencesLiveData(preferences, fieldKey, defaultValue)
+        }
+        return storedValue!!
+    }
+
+}
+
+internal class SharedPreferencesLiveData<T>(
+    private val preferences: SharedPreferences,
+    private val fieldKey: String,
+    private val defaultValue: T
+) : LiveData<T>() {
+
+    private val preferenceChangeListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { _, shKey ->
+            if (shKey == fieldKey) {
+                value = readValue(defaultValue)
+            }
+        }
+
+    override fun onActive() {
+        super.onActive()
+        value = readValue(defaultValue)
+        preferences.registerOnSharedPreferenceChangeListener(preferenceChangeListener)
+    }
+
+    override fun onInactive() {
+        preferences.unregisterOnSharedPreferenceChangeListener(preferenceChangeListener)
+        super.onInactive()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun readValue(defaultValue: T): T {
+        return when (defaultValue) {
+            is Int -> preferences.getInt(fieldKey, defaultValue as Int) as T
+            is Long -> preferences.getLong(fieldKey, defaultValue as Long) as T
+            is Float -> preferences.getFloat(fieldKey, defaultValue as Float) as T
+            is String -> preferences.getString(fieldKey, defaultValue as String) as T
+            is Boolean -> preferences.getBoolean(fieldKey, defaultValue as Boolean) as T
+            else -> error("This type can not be stored into Preferences")
+        }
+    }
+
 }
--- app/src/main/java/ru/skillbranch/skillarticles/ui/dialogs/ChoseCategoryDialog.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/dialogs/ChoseCategoryDialog.kt
@@ -4,11 +4,21 @@ import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_category_dialog.*
+import kotlinx.android.synthetic.main.item_category_dialog.view.*
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.data.local.entities.CategoryData
 import ru.skillbranch.skillarticles.viewmodels.articles.ArticlesViewModel
@@ -72,3 +82,55 @@ data class CategoryDataItem(
 )
 
 fun CategoryData.toItem(isChecked: Boolean = false) = CategoryDataItem(categoryId, icon, title, articlesCount, isChecked)
+
+class CategoryAdapter(private val listener: (String, Boolean) -> Unit) : ListAdapter<CategoryDataItem, CategoryVH>(CategoryDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryVH {
+        val containerView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_category_dialog, parent, false)
+        return CategoryVH(containerView)
+    }
+
+    override fun onBindViewHolder(holder: CategoryVH, position: Int) {
+        holder.bind(getItem(position), listener)
+    }
+}
+
+
+class CategoryVH(
+    override val containerView: View
+) : RecyclerView.ViewHolder(containerView),
+    LayoutContainer {
+
+    fun bind(
+        item: CategoryDataItem,
+        listener: (String, Boolean) -> Unit
+    ) {
+        ch_select.setOnCheckedChangeListener(null)
+
+        ch_select.isChecked = item.isChecked
+        Glide.with(containerView.context)
+            .load(item.icon)
+            .apply(RequestOptions.circleCropTransform())
+            .override(containerView.iv_icon.width)
+            .into(containerView.iv_icon)
+        tv_category.text = item.title
+        tv_count.text = "${item.articlesCount}"
+
+        ch_select.setOnCheckedChangeListener { _, checked -> listener(item.categoryId, checked) }
+        itemView.setOnClickListener { ch_select.toggle() }
+
+
+    }
+
+}
+
+class CategoryDiffCallback : DiffUtil.ItemCallback<CategoryDataItem>() {
+
+    override fun areItemsTheSame(oldItem: CategoryDataItem, newItem: CategoryDataItem): Boolean =
+        oldItem.categoryId == newItem.categoryId
+
+    override fun areContentsTheSame(oldItem: CategoryDataItem, newItem: CategoryDataItem): Boolean =
+        oldItem == newItem
+
+}
